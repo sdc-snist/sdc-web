a59ff3d79244d06229bfeb17619521f68114a876
SiddharthaArepalli <asiddharthagoud@gmail.com>
2025-09-11 10:01:36 +0530

    final changes in the UI
    final changes in the UI

--- src/components/Uxplosion.jsx
+++ src/components/Uxplosion.jsx
@@ -352,7 +352,7 @@ function Uxplosion() {
                   <div className="flex items-center space-x-3">
                     <Mail className="w-5 h-5 text-orange-400" />
                     <span className="text-gray-300" style={{ fontFamily: 'Gilroy, Inter, system-ui, sans-serif' }}>
-                      contact@sdcindia.tech
+                      sdc@sreenidhi.edu.in
                     </span>
                   </div>
                   <div className="flex items-center space-x-3">
@@ -392,7 +392,7 @@ function Uxplosion() {
                   <div className="flex items-center space-x-3">
                     <Clock className="w-5 h-5 text-pink-400" />
                     <span className="text-gray-300" style={{ fontFamily: 'Gilroy, Inter, system-ui, sans-serif' }}>
-                      9:00 AM - 5:00 PM
+                      9:00 AM - 4:00 PM
                     </span>
                   </div>
                 </div>
